2668d7eb56a8198f3e06729b09a6a3dcfd136794
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2024-12-21 14:12:21 +0600

    task_9

---
+++ task_9.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var num float64 — объявление переменной для вещественного числа.
+	var num float64
+	// fmt.Scan(&num) — считывает введенное число с плавающей точкой.
+	fmt.Scan(&num)
+}
