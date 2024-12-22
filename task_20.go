81d2e9cf35d53f548954e3cf57370373c33281cc
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2024-12-22 10:17:42 +0600

    task_20

---
+++ task_20.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(12 % 3)
+	fmt.Println(12 % 4)
+	fmt.Println(12 % 5)
+	fmt.Println(12 % 6)
+	fmt.Println(12 % 7)
+	fmt.Println(12 % 12)
+	fmt.Println(12 % 14)
+}
