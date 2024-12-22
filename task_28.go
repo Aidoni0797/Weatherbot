fe0af0497b31fd0c9a5d45f91dc54190ddf47ccf
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2024-12-22 10:35:55 +0600

    task_28

---
+++ task_28.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var iDONi1 int
+	var iDONi2 int
+	var iDONi3 int
+	fmt.Scan(&iDONi1)
+	fmt.Scan(&iDONi2)
+	fmt.Scan(&iDONi3)
+	fmt.Println(iDONi1 * iDONi2 * iDONi3)
+}
