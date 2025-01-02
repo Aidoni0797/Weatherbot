28a6f05ff4240311d4fe694ae94c0d2f7489ba79
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-02 14:15:59 +0600

    task_179

---
+++ task_179.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var target string
+	fmt.Scan(&target) // Читаем строку s
+
+	// Создаем сканер для считывания ввода строк
+	scanner := bufio.NewScanner(os.Stdin)
+	count := 1 // Начинаем с первого элемента
+
+	for scanner.Scan() {
+		line := scanner.Text() // Считываем строку из ввода
+
+		// Если строка совпала с искомой, выводим номер и завершаем
+		if line == target {
+			fmt.Println(count - 1)
+			return
+		}
+		count++ // Увеличиваем номер
+	}
+}
