109ec67e81c6cb15d849482aafa65125b94e63f8
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-02 14:39:14 +0600

    task_182

---
+++ task_182.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n, c, d int
+	fmt.Scan(&n)
+	fmt.Scan(&c)
+	fmt.Scan(&d)
+
+	// Проходим по всем числам от 1 до n
+	for i := 1; i <= n; i++ {
+		// Если число кратно c, но не кратно d, выводим его
+		if i%c == 0 && i%d != 0 {
+			fmt.Println(i, " ")
+		}
+	}
+}
