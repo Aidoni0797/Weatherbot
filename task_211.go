58f026bf7c8b920452bdca4f41643e52252cafb1
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-07 00:38:13 +0500

    task_211.go

---
+++ task_211.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var size int
+	fmt.Scan(&size)                     // считываем сколько чисел будем вводить с клавиатуры
+	numbers := make([]int, size)        // заводим новый slice целого типа размерностью кол-ва чисел, которые будем вводить с клавиатуры
+	for i := 0; i < len(numbers); i++ { // получаем длину slice-а и проходим по нему циклом
+		fmt.Scan(&numbers[i]) // считываем и сразу записываем значение в i-ый индекс slice-а
+	}
+	fmt.Println(numbers)
+}
