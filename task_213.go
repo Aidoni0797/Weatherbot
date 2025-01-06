d5e1d51096ddd1730c553a86044e0008930ab555
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-07 00:39:34 +0500

    task_213

---
+++ task_213.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := make([]int, 5)
+	numbers[0] = 1
+	numbers[1] = 2
+	numbers[2] = 3
+	numbers[3] = 4
+	numbers[4] = 5
+	fmt.Println(numbers)
+}
